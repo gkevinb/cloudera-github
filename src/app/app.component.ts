d25330ac8ece55ca087b7738faf58f3199e4cb40
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-08-08 18:27:04 +0200

    Get JSON from Github API and place data in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cloudera-github-app';
+  searchValue = '';
+  database = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  onKey(event: any) {
+    this.searchValue = event.target.value;
+  }
+
+  getGithubRepos() {
+    if(this.searchValue != ''){
+      console.log('get repos!')
+      this.httpClient.get('https://api.github.com/search/repositories?q=' + this.searchValue).subscribe((res)=>{
+        console.log(res)
+        this.database = []
+        for(let i = 0; i < res['items'].length; i++){
+          let json_data = {}
+          console.log(res['items'][i])
+          json_data['name'] = res['items'][i].name
+          json_data['html_url'] = res['items'][i].html_url
+          json_data['description'] = res['items'][i].description
+          json_data['forks_count'] = res['items'][i].forks_count
+          json_data['stargazers_count'] = res['items'][i].stargazers_count
+          json_data['open_issues_count'] = res['items'][i].open_issues_count
+          this.database.push(json_data)
+        }
+        console.log(this.database)
+      });
+    }else{
+      console.log('search value empty')
+      this.database = []
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './result/result.component';
+import { IssuesComponent } from './issues/issues.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent,
+    IssuesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
