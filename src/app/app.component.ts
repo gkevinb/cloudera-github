6fba8a503175ce424ed98b21e3e3aca95483cba0
Gabor Kevin Barta <gkbarta@yahoo.com>
2019-08-08 22:24:22 +0200

    Preliminary work on layout and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cloudera-github-app';
   searchValue = '';
   database = [];
   issues = [];
@@ -23,42 +22,37 @@ export class AppComponent {
 
   getGithubRepos() {
     if(this.searchValue != ''){
-      console.log('get repos!')
       this.httpClient.get('https://api.github.com/search/repositories?q=' + this.searchValue).subscribe((res)=>{
-        console.log(res)
-        this.database = []
+        this.database = [];
         for(let i = 0; i < res['items'].length; i++){
-          let json_data = {}
-          json_data['name'] = res['items'][i].name
-          json_data['full_name'] = res['items'][i].full_name
-          json_data['html_url'] = res['items'][i].html_url
-          json_data['description'] = res['items'][i].description
-          json_data['forks_count'] = res['items'][i].forks_count
-          json_data['stargazers_count'] = res['items'][i].stargazers_count
-          json_data['open_issues_count'] = res['items'][i].open_issues_count
-          this.database.push(json_data)
+          let json_data = {};
+          json_data['name'] = res['items'][i].name;
+          json_data['full_name'] = res['items'][i].full_name;
+          json_data['html_url'] = res['items'][i].html_url;
+          json_data['description'] = res['items'][i].description;
+          json_data['forks_count'] = res['items'][i].forks_count;
+          json_data['stargazers_count'] = res['items'][i].stargazers_count;
+          json_data['open_issues_count'] = res['items'][i].open_issues_count;
+          this.database.push(json_data);
         }
-        console.log(this.database)
       });
     }else{
-      console.log('search value empty')
-      this.database = []
+      this.database = [];
     }
   }
   showIssues(i: number) {
-    this.issues = []
+    this.issues = [];
     console.log("Displaying " + i.toString() + " repo issue")
     this.httpClient.get('https://api.github.com/search/issues?q=repo:' + this.database[i].full_name + '+state:open').subscribe((res)=>{
         for(let i = 0; i < res['items'].length; i++){
-          let json_data = {}
-          json_data['number'] = res['items'][i].number
-          json_data['title'] = res['items'][i].title
-          json_data['updated_at'] = res['items'][i].updated_at
-          json_data['state'] = res['items'][i].state
-          json_data['url'] = res['items'][i].url
-          this.issues.push(json_data)
+          let json_data = {};
+          json_data['number'] = res['items'][i].number;
+          json_data['title'] = res['items'][i].title;
+          json_data['updated_at'] = res['items'][i].updated_at;
+          json_data['state'] = res['items'][i].state;
+          json_data['url'] = res['items'][i].url;
+          this.issues.push(json_data);
         }
-        console.log(this.issues)
       });
   }
 }
